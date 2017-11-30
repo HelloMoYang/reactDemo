4754e8b556ab8297091ad58846e9339057036ef7
杨沫 <374577605@qq.com>
2017-12-01 00:23:10 +0800

    基础react环境完成

---
+++ app/components/Component.jsx
@@ -0,0 +1,15 @@
+//Component1.jsx
+import React from "react";
+
+const user = {
+  firstname: "yyy",
+  lastname: "xxx"
+};
+class Component1 extends React.Component {
+  render() {
+    return <div>Hello, MoYang</div>;
+  }
+}
+
+//导出组件
+export default Component1;
