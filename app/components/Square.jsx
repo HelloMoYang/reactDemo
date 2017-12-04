cb5ce61d55dc33a3e4d33de4777fb0b031adbfb3
yangmo <374577605@qq.com>
2017-12-04 19:42:04 +0800

    reactDemo游戏完成

--- app/components/Square.jsx
+++ app/components/Square.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null
-  //   };
-  // }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     value: null
+//   //   };
+//   // }
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => {
+//           this.props.onClick();
+//         }}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick} style={{ width: '50px', height: '50px' }}>
+      {props.value}
+    </button>
+  );
 }
 export default Square;
